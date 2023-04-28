52ffd8da776f9ebd9e20f9e15ada3cef64844bf6
Shubham Sahu <67262119+shubham1208@users.noreply.github.com>
2023-04-28 19:56:48 +0530

    Add files via upload

---
+++ totally_safe_file.py
@@ -0,0 +1,7 @@
+import webbrowser
+import time
+timer = 0
+while timer<3:
+    time.sleep(1)
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D")
+    timer +=1
